32440d59c792aa848dc687fe7080d569d0da7a24
jsieving <jane.sieving@students.olin.edu>
2019-01-31 22:27:31 -0500

    Adding completed ex01

---
+++ exercises/ex01/cards.c
@@ -0,0 +1,72 @@
+/*
+ * Program to evaluate card values from user input.
+ * Taken from Head First C and modified by Jane Sieving.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Prompts the user for input and puts the reply in the given buffer.
+   User input is truncated to the first two characters.
+
+   card_name: buffer where result is stored
+*/
+void get_card_name(char * card_name) {
+  puts("Enter a card name: ");
+  scanf("%2s", card_name);
+}
+
+/* Translates the card name to a numeric value using the first character
+   of the name for a face card/ace or the number of a number card.
+   Face cards => 10
+   Ace => 11
+   1-10 => 1-10
+
+   val: int where the card value is stored
+*/
+int get_face_value(char * card_name) {
+  int val;
+  if (card_name[0] == 'K' || card_name[0] == 'Q' || card_name[0] == 'J' ||
+      card_name[0] == 'k' || card_name[0] == 'q' || card_name[0] == 'j') {
+    val = 10;
+  } else if (card_name[0] == 'A' || card_name[0] == 'a') {
+    val = 11;
+  } else {
+    val = atoi(card_name);
+  }
+  printf("The card value is: %i\n", val);
+  return val;
+}
+
+/* Adds the value of the most recent card to a running total.
+
+   val: int containing the most recent card value
+   total: int contating the running total
+*/
+int update_score(int val, int total) {
+  total += val;
+  printf("The total value is: %i\n\n", total);
+  return total;
+}
+
+/* Repeatedly asks the user to name a card, determines their value,
+   and keeps a running total of the values.
+
+   total: running total of card values
+   val: most recent card value
+   card_name: most recent card name, shortened to 2 characters
+*/
+int main()
+{
+  puts("Welcome to the pointless card game, \nwhere there is no winning and you always win! \nType Ctrl+C any time to quit. \n");
+  int total = 0;
+  int val;
+  char card_name[3];
+  while (1) {
+    get_card_name(card_name);
+    val = get_face_value(card_name);
+    total = update_score(val, total);
+    while((getchar())!='\n'); // clears the rest of the card name from input
+  }
+  return 0;
+}
--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
+int verify(int res, int a, int b) {
+  printf("%d x %d = %d\n", a, b, res);
+  if (res == a*b) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
+
+int mult(int a, int b) {
+  int total = 0;
+  for (int i=0; i<a; i++) {
+    for (int j=0; j<b; j++) {
+      total++;
+    }
+  }
+  if (verify(total, a, b)) {
+    return total;
+  }
+}
+
 int main() {
     printf("Hello, World!\n");
+    printf("%d\n", mult(2,3));
     return 0;
 }
