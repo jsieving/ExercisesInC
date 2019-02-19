2d01ebfed6ba70feae06aa845a75af25672abdd0
jsieving <jane.sieving@students.olin.edu>
2019-02-19 12:56:42 -0500

    Adding find_track

--- exercises/ex02.5/find_track.c
+++ exercises/ex02.5/find_track.c
@@ -7,6 +7,7 @@ Modified version of an example from Chapter 2.5 of Head First C.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 
 #define NUM_TRACKS 5
 
@@ -37,7 +38,36 @@ void find_track(char search_for[])
 // Prints track number and title.
 void find_track_regex(char pattern[])
 {
-    // TODO: fill this in
+    int i;
+    int errno;
+    char errbuf[80];
+    regex_t preg;
+    int res;
+    int match = 0;
+    errno = regcomp(&preg, pattern, REG_EXTENDED | REG_NOSUB);
+    /* Handle errors from regcomp, or search tracks for the compiled regexp. */
+    if (errno) {
+        regerror(errno, &preg, errbuf, sizeof(errbuf));
+        printf("%s\n", errbuf);
+        exit(1);
+    } else {
+        for (i=0; i<NUM_TRACKS; i++) {
+            res = regexec(&preg, tracks[i], 0, NULL, 0);
+            if (res == 0) {
+                printf("Track %i: '%s'\n", i, tracks[i]);
+                match = 1;
+            /* Handle errors except NOMATCH - we don't always expect a match. */
+            } else if (res != REG_NOMATCH) {
+              regerror(res, &preg, errbuf, sizeof(errbuf));
+              printf("%s\n", errbuf);
+              exit(1);
+            }
+        }
+        if (match == 0) {
+            puts("No matches found.");
+        }
+    }
+    regfree(&preg);
 }
 
 // Truncates the string at the first newline, if there is one.
@@ -49,6 +79,9 @@ void rstrip(char s[])
     }
 }
 
+// Prompts the user to search for a string or regular expression,
+// then prints any tracks containing the string or matching the
+// regular expression.
 int main (int argc, char *argv[])
 {
     char search_for[80];
@@ -58,8 +91,8 @@ int main (int argc, char *argv[])
     fgets(search_for, 80, stdin);
     rstrip(search_for);
 
-    find_track(search_for);
-    //find_track_regex(search_for);
+    // find_track(search_for);
+    find_track_regex(search_for);
 
     return 0;
 }
