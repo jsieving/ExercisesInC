9a42ef2091b1ddf2aec7ea2c41ad520240d6e2ce
jsieving <jane.sieving@students.olin.edu>
2019-02-27 23:45:00 -0500

    Updating trout

---
+++ exercises/ex04/trout/trout.h
@@ -0,0 +1,50 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <time.h>
+#include <netinet/in.h>
+#include <netinet/in_systm.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <netinet/udp.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <sys/un.h>
+#include <stdarg.h>
+#include <syslog.h>
+#include <unistd.h>
+
+#define MAXLINE 4096
+#define BUFSIZE 1500
+
+typedef struct timeval Timeval;
+typedef struct sockaddr Sockaddr;
+
+Sockaddr *sasend;    /* socket addresses for various purposes */
+Sockaddr *sarecv;
+Sockaddr *salast;
+Sockaddr *sabind;
+
+socklen_t salen;                    /* length of a socket address */
+
+/* the following are a few definitions from Stevens' unp.h */
+
+typedef	void Sigfunc(int);        /* for signal handlers */
+
+#define max(a,b) ((a) > (b) ? (a) : (b))
+
+void sig_alrm (int signo);
+int process_ip (struct ip *ip, int len);
+int recv_dgram ();
+void sub_tv (Timeval *plus, Timeval *minus, Timeval *res);
+double time_to_double (Timeval *time);
+void print_report ();
+void send_dgram (int ttl);
+int send_probes (int ttl);
+void loop_ttl ();
