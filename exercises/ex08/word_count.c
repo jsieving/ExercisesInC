8007b13eb6c79dd780fd7d8228c86c7c426c3c7c
jsieving <jane.sieving@students.olin.edu>
2019-04-11 20:42:09 -0400

    fix problems with solution

--- exercises/ex08/word_count.c
+++ exercises/ex08/word_count.c
@@ -3,11 +3,11 @@
 #include <fcntl.h>
 
 typedef struct {
-  gint count;
+  gint* count;
   gchar* word;
 } Pair;
 
-Pair* make_pair(gchar* word, gint count) {
+Pair* make_pair(gchar* word, gint* count) {
   Pair* pair = malloc(sizeof(Pair));
   pair->count = count;
   pair->word = word;
@@ -18,21 +18,25 @@ void free_pair(void* pair) {
   free((Pair*)pair);
 }
 
-void enum_kvs(void* key, void* value, void* dest_list) {
-  Pair* pair = make_pair((gchar*) key, *(gint*) value);
-  g_sequence_append((GSequence*) dest_list, pair);
-}
-
 gint com_pair(const void* a, const void* b, void* data) { // WHY does it require 3 params?
-  return ((Pair*)a)->count - ((Pair*)b)->count;
+  gint* a_count = ((Pair*)a)->count;
+  gint* b_count = ((Pair*)b)->count;
+  return GPOINTER_TO_INT(b_count) - GPOINTER_TO_INT(a_count);
 }
 
 void print_pair(void* pair, void* data) { // This one too! Why no variadic functions??
-  printf("%d\t| %s\n", ((Pair*)pair)->count, ((Pair*)pair)->word);
+  gint* count = ((Pair*)pair)->count;
+  gchar* word = ((Pair*)pair)->word;
+  printf("%d\t| '%s'\n", GPOINTER_TO_INT(count), word);
+}
+
+void enum_kvs(void* key, void* value, void* dest_list) {
+  Pair* pair = make_pair((gchar*) key, (gint*) value);
+  g_sequence_append((GSequence*) dest_list, pair); //key should be pair
 }
 
 int main() {
-  gint* count;
+  long int count;
   GHashTable* histogram = g_hash_table_new(g_str_hash, g_str_equal);
   gchar *filename = "test.txt";
   gchar *text;
@@ -44,13 +48,15 @@ int main() {
     puts("Error in main: g_file_get_contents: failed to read file.");
     return 1;
   }
-  word_list = g_strsplit_set(text, " ,.?!-", 0);
+  word_list = g_strsplit_set(text, " ,.?!-\n", 0);
   int i = 0;
   while (word_list[i] != NULL) {
-    count = (gint*)(g_hash_table_lookup(histogram, word_list[i]));
-    gint newcount = (*count) + 1;
-    puts(word_list[i]);
-    g_hash_table_insert(histogram, word_list[i], &newcount);
+    if (word_list[i][0] != '\0') {
+      count = (long int) g_hash_table_lookup(histogram, word_list[i]);
+      count++;
+      g_hash_table_insert(histogram, word_list[i], GINT_TO_POINTER(count));
+    }
+    i++;
   }
   g_hash_table_foreach(histogram, enum_kvs, pairs);
   g_sequence_sort(pairs, com_pair, NULL);
